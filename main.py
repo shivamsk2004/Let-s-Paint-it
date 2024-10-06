fc9f017e24d47879558cf74f03ce5dfa270a8169
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2024-10-06 10:59:19 +0530

    created basic pencil

--- main.py
+++ main.py
@@ -18,11 +18,22 @@ canvas.grid(row=0,column=0)
 #canvas.create_oval(100,100,120,120,fill="black")
 #basically line from (x,y) to (x',y')
 
+#variables for pencil
+
+prevPoint = [0,0]
+currentPoint = [0,0]
 def paint(event):
+    global prevPoint
+    global currentPoint
     x=event.x
     y=event.y
-    canvas.create_oval(x,y,x+20,y+20,fill="black")
+    currentPoint = [x,y]
+    #canvas.create_oval(x,y,x+2,y+2,fill="black")
+    if prevPoint!= [0,0] : #ab top left corner se start nhi hoga
+        canvas.create_line(prevPoint[0],prevPoint[1],currentPoint[0],currentPoint[1])
 
+    prevPoint=currentPoint
+        
 canvas.bind("<B1-Motion>",paint)
 #canvas.bind("<Button-1>",paint)
 #mouse ke left button ke saath connect hogaya hai
