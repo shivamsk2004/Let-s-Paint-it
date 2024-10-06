4cfc184085e441a95ca50480e9e3674ef6d7e242
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2024-10-06 11:53:29 +0530

    Stroke size

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ root=Tk()
 root.title("Let's Paint")
 root.geometry("1100x600")
 
-frame1 = Frame(root,height=100,width=1100,bg="red")
+frame1 = Frame(root,height=100,width=1100)
 frame1.grid(row=0,column=0,sticky=NW)
 
-toolsFrame = Frame(frame1, height=100, width=100, bg="green")
+toolsFrame = Frame(frame1, height=100, width=100)
 toolsFrame.grid(row=0,column=0)
 
 def usePencil():
     stroke_color.set("black")
-
+    canvas["cursor"]="arrow"
 def useEraser():
     stroke_color.set("white")
     canvas["cursor"]=DOTBOX
@@ -25,6 +25,25 @@ eraserButton.grid(row=1,column=0)
 
 toolsLabel=Label(toolsFrame,text="Tools",width=10)
 toolsLabel.grid(row=2,column=0)
+
+sizeFrame=Frame(frame1,height=100,width=100)
+sizeFrame.grid(row=0,column=1)
+
+defaultButton = Button(sizeFrame,text="Default",width=10,command=usePencil)
+defaultButton.grid(row=0,column=0)
+
+stroke_size = IntVar()
+options = [1,2,3,4,5]
+
+sizeList=OptionMenu(sizeFrame,stroke_size,*options)
+sizeList.grid(row=1,column=0)
+
+sizeLabel=Label(sizeFrame,text="Size",width=10)
+sizeLabel.grid(row=2,column=0)
+
+stroke_color=StringVar()
+stroke_color.set("green")
+
 #tkinter mein rows,columns by default 0 se start hoti hai
 #frames matlab alag alag blocks
 frame2 = Frame(root,height=500,width=1100,bg="yellow")
@@ -32,14 +51,10 @@ frame2.grid(row=1,column=0)
 
 canvas=Canvas(frame2,height=500,width=1100,bg="white")
 canvas.grid(row=0,column=0)
-
 #canvas.create_line(100,100,200,200)
 #canvas.create_oval(100,100,120,120,fill="black")
 #basically line from (x,y) to (x',y')
-
 #variables for pencil
-stroke_color=StringVar()
-stroke_color.set("green")
 
 prevPoint = [0,0]
 currentPoint = [0,0]
